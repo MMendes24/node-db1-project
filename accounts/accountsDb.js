dd3a6db7cfdf7896310bc43a4ff9b2e50724cc16
MMendes24 <mmbusiness1@protonmail.com>
2020-09-03 15:17:55 -0400

    GET methods built out.

--- accounts/accountsDb.js
+++ accounts/accountsDb.js
@@ -2,10 +2,20 @@ const db = require('../data/dbConfig');
 
 module.exports = {
     get,
+    getById,
+    insert,
+    update,
+    remove,
 }
 
 function get(){
     return db('accounts')
+}
+
+function getById(id) {
+    return db('accounts')
+    .where({ id })
+    .first()
 }
 
 function insert(account) {
--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -13,4 +13,14 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    Accounts.getById(req.params.id)
+    .then(thenRes => {
+        res.status(200).json(thenRes)
+    })
+    .catch(err => {
+        res.status(404).json({ errorMessage: "No"})
+    })
+})
+
 module.exports = router
